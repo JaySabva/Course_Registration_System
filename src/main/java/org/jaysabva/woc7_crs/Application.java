720b382c1fa4e8fe2302c73762b3fcabf826fab8
JaySabva <202101224@daiict.ac.in>
2025-01-20 15:56:06 +0530

    db Connection Check

--- src/main/java/org/jaysabva/woc7_crs/Application.java
+++ src/main/java/org/jaysabva/woc7_crs/Application.java
@@ -2,8 +2,10 @@ package org.jaysabva.woc7_crs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.jaysabva.woc7_crs")
 public class Application {
 
     public static void main(String[] args) {
